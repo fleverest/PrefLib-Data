5691fd73546fc1c9de9748d1d0232c8ada2df786
Simon Rey <reysimon@orange.fr>
2022-09-16 19:32:55 +0200

    Some relates_to fields missing

--- scripts/correctors/infofile_relatesto.py
+++ scripts/correctors/infofile_relatesto.py
@@ -1,12 +1,15 @@
 
-dataset = "sf"
+dataset = "00011 - web"
 new_str = ""
+
+original_type = 'soi'
 with open("../../datasets/" + dataset + "/info.txt", "r") as file:
     for line in file.readlines():
         line = line.strip()
         if "imbued" in line:
             split = line.split(',')
-            new_str += "{},{}, {},{}, {},{}\n".format(split[0], split[1], split[0].split('.')[0] + '.toi', split[3], 'Obtained from the toi by adding the unranked alternatives at the bottom', split[5])
+            new_str += "{},{}, {},{}, {},{}\n".format(split[0], split[1], split[0].split('.')[0] + '.' + original_type, split[3],
+                                                      'Obtained from the ' + original_type + ' by adding the unranked alternatives at the bottom', split[5])
         else:
             new_str += line + "\n"
 
